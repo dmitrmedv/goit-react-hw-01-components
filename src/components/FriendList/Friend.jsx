4c5b2cffc9e0a6530a862ff852a49ac9631719e5
dmitrmedv <dmitrmedv@gmail.com>
2023-06-02 08:28:42 +0300

    Update Friend.jsx

--- src/components/FriendList/Friend.jsx
+++ src/components/FriendList/Friend.jsx
@@ -4,7 +4,7 @@ import { Item, Status, Avatar, Name } from './FriendList.styles';
 export const Friend = ({ avatar, name, isOnline }) => {
   return (
     <Item>
-      <Status isOnline={isOnline}>{isOnline}</Status>
+      <Status isOnline={isOnline}></Status>
       <Avatar src={avatar} alt={name} width="48" />
       <Name>{name}</Name>
     </Item>
