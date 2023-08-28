60ed1ffb3eb50460ad27e21426ebf4b167e1a4e2
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2023-08-28 12:58:03 +0300

    add

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,29 @@
 export const Profile = () => {
   return (
-    <div class="profile">
-      <div class="description">
+    <div className="profile">
+      <div className="description">
         <img
           src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
           alt="User avatar"
-          class="avatar"
+          className="avatar"
         />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+        <p className="name">Petra Marica</p>
+        <p className="tag">@pmarica</p>
+        <p className="location">Salvador, Brasil</p>
       </div>
 
-      <ul class="stats">
+      <ul className="stats">
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+          <span className="label">Followers</span>
+          <span className="quantity">1000</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+          <span className="label">Views</span>
+          <span className="quantity">2000</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+          <span className="label">Likes</span>
+          <span className="quantity">3000</span>
         </li>
       </ul>
     </div>
